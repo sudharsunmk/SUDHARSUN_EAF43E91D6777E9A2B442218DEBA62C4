e3e10dd1e873a0ca3e240751d4245de26681a263
sudharsunmk <144462736+sudharsunmk@users.noreply.github.com>
2023-09-26 19:32:35 +0530

    Add files via upload

---
+++ Challenge 3.2.py
@@ -0,0 +1,25 @@
+'''Implement a function called sort_students that takes a list of student objects as input and sorts the list based on their CGPA (Cumulative Grade Point Average) in descending order. Each student object has the following attributes: name (string), roll_number (string), and cgpa (float). Test the function with different input lists of students.'''
+
+
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+def sort_students(student_list):
+    # Sort the student list based on CGPA in descending order
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
+
+# Test the function with different input lists of students
+students = [
+    Student("Ramesh", "3040", 3.7),
+    Student("Mathav", "3028", 3.9),
+    Student("Akash", "3001", 3.5)
+]
+
+sorted_students = sort_students(students)
+
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")

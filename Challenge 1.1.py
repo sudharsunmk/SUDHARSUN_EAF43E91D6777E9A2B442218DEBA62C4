20ce204ae6de132a88cb9959ba36ba8f36513788
sudharsunmk <144462736+sudharsunmk@users.noreply.github.com>
2023-09-26 19:54:24 +0530

    Add files via upload

---
+++ Challenge 1.1.py
@@ -0,0 +1,18 @@
+# Implement a recursive function to calculate the factorial of a given number.
+def factorial(n):
+  if n == 0:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+# Input from the user
+num = int(input("Enter a number: "))
+
+if num < 0:
+  print("Factorial is not defined for negative numbers.")
+elif num == 0:
+  print("The factorial of 0 is 1")
+else:
+  result = factorial(num)
+  print(f"The factorial of {num} is {result}")
